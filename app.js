8ca6bf6f8ca259eb6d73fc327b45b2bee11a052c
Dennis <d.matveev@bigmir.net>
2017-05-09 18:18:25 +0300

    emailing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var formidable = require('formidable' );
+var connect = require(connect);
+var nodemailer = require('nodemailer');
 
 var fortunes = require('./lib/fortunes.js');
 var weatherData = require('./lib/weather.js');
@@ -7,6 +9,14 @@ var cartValidation = require('./lib/cartValidation.js');
 var credentials = require('./credentials.js');
 
 var app = express();
+
+var mailTransport = nodemailer.createTransport('SMTP',{
+    service: 'Gmail',
+    auth: {
+        user: credentials.gmail.user,
+        pass: credentials.gmail.password
+    }
+});
 
 // template engine Handlebars setting with custom helper
 var handlebars = require('express-handlebars').create({ defaultLayout:'main',
@@ -171,6 +181,38 @@ app.post('/contest/vacation-photo/:year/:month' , function(req, res){
         console.log(files);
         res.redirect(303, '/thank-you' );
     });
+});
+
+app.post('/cart/checkout', function(req, res){
+    var cart = req.session.cart;
+    if(!cart) next(new Error('Корзина не существует.'));
+    var name = req.body.name || '', email = req.body.email || '';
+// Проверка вводимых данных
+    if(!email.match(VALID_EMAIL_REGEX))
+        return res.next(new Error('Некорректный адрес электронной почты.'));
+// Присваиваем случайный идентификатор корзины;
+// При обычных условиях мы бы использовали
+// здесь идентификатор из БД
+    cart.number = Math.random().toString().replace(/^0\.0*/, '');
+    cart.billing = {
+        name: name,
+        email: email
+    };
+    res.render('email/cart-thank-you',
+        { layout: null, cart: cart }, function(err,html){
+            if( err ) console.log('ошибка в шаблоне письма');
+            mailTransport.sendMail({
+                from: '"EnjoyTravel": info@enjoytravel.com',
+                to: cart.billing.email,
+                subject: 'Спасибо за заказ поездки',
+                html: html,
+                generateTextFromHtml: true
+        }, function(err){
+                if(err) console.error('Не могу отправить подтверждение: ' + err.stack);
+            });
+        }
+    );
+    res.render('cart-thank-you', { cart: cart });
 });
 
 // routes for demo of client-side Handlebars realization
