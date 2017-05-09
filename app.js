37295963390a71b62e0b6812582cd8295f8bd09e
Dennis <d.matveev@bigmir.net>
2017-05-09 16:50:49 +0300

    flash and session

--- app.js
+++ app.js
@@ -51,6 +51,15 @@ app.use(function(req, res, next){
     next();
 });
 
+// middleware for flash messages
+app.use(function(req, res, next){
+    // Если имеется экстренное сообщение,
+    // переместим его в контекст и удалим
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
+    next();
+});
+
 // routes
 app.get('/', function(req, res){
     res.render('home');
@@ -67,6 +76,49 @@ app.get('/tours/hood-river', function(req, res){
 
 app.get('/tours/request-group-rate', function(req, res){
     res.render('tours/request-group-rate');
+});
+
+// Немного измененная версия официального регулярного выражения
+// W3C HTML5 для электронной почты:
+// https://html.spec.whatwg.org/multipage/forms.html#valid-e-mail-address
+var VALID_EMAIL_REGEX = new RegExp('^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@' +
+    '[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?' +
+    '(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$');
+
+app.post('/newsletter', function(req, res){
+    var name = req.body.name || '', email = req.body.email || '';
+
+    if(!email.match(VALID_EMAIL_REGEX)) {
+        if(req.xhr) return res.json({ error: 'Некорректный адрес электронной почты.' });
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Ошибка проверки!',
+            message: 'Введенный вами адрес электронной почты некорректен.'
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+
+    new NewsletterSignup({ name: name, email: email }).save(function(err) {
+        
+        if(err) {
+            if(req.xhr) return res.json({ error: 'Ошибка базы данных.' });
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Ошибка базы данных!',
+                message: 'Произошла ошибка базы данных. Пожалуйста, попробуйте позднее'
+            };
+            return res.redirect(303, '/newsletter/archive');
+        }
+
+        if(req.xhr) return res.json({ success: true });
+
+        req.session.flash = {
+            type: 'success',
+            intro: 'Спасибо!',
+            message: 'Вы были подписаны на информационный бюллетень.'
+        };
+        return res.redirect(303, '/newsletter/archive');
+    });
 });
 
 // page with subsribe form
