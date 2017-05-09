737751453fadb813ae3c090cc896137e0a92fa53
Dennis <d.matveev@bigmir.net>
2017-05-09 18:45:20 +0300

    logging added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var formidable = require('formidable' );
-// var connect = require(connect);
+var connect = require('connect');
 // var nodemailer = require('nodemailer');
 
 var fortunes = require('./lib/fortunes.js');
@@ -47,6 +47,21 @@ app.use(require('express-session')({
     saveUninitialized: false,
     secret: credentials.cookieSecret
 }));
+
+// choose logging tool depending on mode
+switch(app.get('env')){
+    case 'development':
+        // сжатое многоцветное журналирование для разработки
+        app.use(require('morgan')('dev'));
+        break;
+    case 'production':
+        // модуль 'express-logger' поддерживает ежедневное
+        // чередование файлов журналов
+        app.use(require('express-logger')({
+            path: __dirname + '/log/requests.log'
+        }));
+        break;
+}
 
 // our custom middleware module; see below more advanced usage
 // app.use(require('./lib/tourRequiresWaiver.js'));
